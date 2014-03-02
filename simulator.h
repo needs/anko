a33c34fc17664ff3e4ccf0672e3cbedc35d54d25
derdaelj <jeremy.derdaele@gmail.com>
2014-03-02 18:25:47 +0100

    updated simulator.h

--- simulator.h
+++ simulator.h
@@ -1,11 +1,28 @@
 #ifndef _SIMULATOR_H_
 #define _SIMULATOR_H_
 
+/*
+  Nb : number of burning neightbours
+  Transition rule :
+
+  if Nb < 1 then
+      BURNABLE -> BURNABLE
+  else
+      BURNABLE -> BURNING (with probability p)
+	  NEUTRAL  -> NEUTRAL
+	  BURNABLE -> BURNED (add burning time ?)
+	  BURNED   -> BURNED
+	  
+  p(Nb) = Nb/8
+*/
+
 typedef enum state_t {
 	NEUTRAL = 0,
 	BURNABLE,
 	BURNING,
 	BURNED,
 } state_t;
+
+void step(state_t** dest, state_t** src, int width, int height);
 
 #endif /* _SIMULATOR_H_ */
