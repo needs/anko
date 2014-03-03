35be7f2dd6d10a255d77d82a1e0f6226d6879dcc
jeremagician <jeremagician@gmail.com>
2014-03-03 16:41:43 +0100

    Graphic: subdivided main

--- src/graphics/main.c
+++ src/graphics/main.c
@@ -15,15 +15,22 @@ static const int WIDTH  = 20;
 static const int HEIGHT = 20;
 static const int STEP_TIMER = 1000;
 
+static unsigned frames = 0;
+static unsigned last_fps = 0;
+static unsigned last_time = 0;
+static unsigned deltatime = 0;
+static unsigned step_current_timer;
+
+static board_t *board, *dest, *tmp;
+static map_t *map;
+
+void simulate();
+void update_fps();
+void sleep();
+
 int main(void)
 {
-	board_t *board, *dest, *tmp;
-	unsigned frames = 0;
-	unsigned last_fps = 0;
-	unsigned last_time = 0;
-	unsigned deltatime = 0;
-	unsigned step_current_timer = STEP_TIMER;
-	map_t *map;
+
 	
 	srandom(time(NULL));
 	
@@ -37,38 +44,18 @@ int main(void)
 	if ((map = create_map(WIDTH, HEIGHT)) == NULL)
 		goto err_map;
 
+	step_current_timer = STEP_TIMER;
+	
 	while(!quit) {
 		last_time = SDL_GetTicks();
 
 		process_events((float)deltatime/1000);
-		if(step_current_timer < deltatime)
-		{
-			step_current_timer = STEP_TIMER - step_current_timer;
-			step(dest, board);
-			tmp = board;
-			board = dest;
-			dest = tmp;
-		}
-		else
-			step_current_timer -= deltatime;
-
-		if(SDL_GetTicks()- last_fps >= 1000)
-		{
-			last_fps = SDL_GetTicks();
-			printf("FPS : %i\n", frames);
-			frames = 0;
-		}
-		else
-			frames++;
-
+		
+		simulate();
+		update_fps();
 		render(board, map);
-
 		deltatime = SDL_GetTicks() - last_time;
-
-		if(deltatime < (unsigned)1000/FPS)
-			SDL_Delay(1000/FPS - deltatime);
-
-		deltatime = SDL_GetTicks() - last_time;
+		sleep();
 	}
 	
 	free_map(map);
@@ -86,4 +73,40 @@ err_board:
 	close_context();
 err_context:
 	return EXIT_FAILURE;
+}
+
+void simulate()
+{
+	if(step_current_timer < deltatime)
+	{
+		step_current_timer = STEP_TIMER - step_current_timer;
+		step(dest, board);
+		tmp = board;
+		board = dest;
+		dest = tmp;
+	}
+	else
+		step_current_timer -= deltatime;
+}
+
+void update_fps()
+{
+	if(SDL_GetTicks()- last_fps >= 1000)
+	{
+		last_fps = SDL_GetTicks();
+		printf("FPS : %i\n", frames);
+		frames = 0;
+	}
+	else
+		frames++;
+		
+}
+
+void sleep()
+{
+	if(deltatime < (unsigned)1000/FPS)
+	{
+		SDL_Delay(1000/FPS - deltatime);
+		deltatime+=1000/FPS - deltatime;
+	}
 }
