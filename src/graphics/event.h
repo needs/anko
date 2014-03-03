2e1669940cbc0f77ea8473cffc85ffd30ed7d324
Needs <needs@mailoo.org>
2014-03-03 20:36:30 +0100

    Remove graphics, start from fresh

--- src/graphics/event.h
+++
@@ -1,6 +0,0 @@
-#ifndef _EVENT_H_
-#define _EVENT_H_
-
-void process_events(float deltatime);
-
-#endif /* _EVENT_H_ */
--- src/graphics/render.h
+++
@@ -1,9 +0,0 @@
-#ifndef _RENDER_H_
-#define _RENDER_H_
-
-#include "map.h"
-#include "../board.h"
-
-void render(board_t *board, map_t *map);
-
-#endif /* _RENDER_H_ */
