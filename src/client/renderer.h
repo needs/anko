2d0f7cb70f737d0575e2ca11a9a74b46b663fd6c
Needs <needs@mailoo.org>
2014-03-03 22:44:38 +0100

    Move rendering primitives to renderer.[ch]

---
+++ src/client/renderer.h
@@ -0,0 +1,8 @@
+#ifndef _RENDERER_H_
+#define _RENDERER_H_
+
+void init_rendering(void);
+void close_rendering(void);
+void render_rect(float x, float y, float width, float height);
+
+#endif /* _RENDERER_H_ */
