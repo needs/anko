a3993cf09021950abbb5a08b43d8a21acc850a19
needs <needs@mailoo.org>
2015-01-15 11:07:57 +0100

    Basic message sending between client and server using UDP

--- src/client/main.c
+++ src/client/main.c
@@ -1,7 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
 #include <shared/network.h>
+
+#define BUFSIZE 128
+
+static int push_network(int fd, char *buf, size_t len)
+{
+	ssize_t ret;
+
+	ret = send(fd, buf, len, 0);
+	if (ret == -1) {
+		fprintf(stderr, "send(%s): %s\n", buf, strerror(errno));
+		return 0;
+	}
+
+	return 1;
+}
 
 int main(int argc, char **argv)
 {
@@ -24,6 +44,11 @@ int main(int argc, char **argv)
 	 *   5) predict(world)
 	 *   6) render(world)
 	 */
+	while (1) {
+		char buf[BUFSIZE];
+		fgets(buf, BUFSIZE, stdin);
+		push_network(fd, buf, strlen(buf));
+	}
 
 	close(fd);
 
--- src/server/main.c
+++ src/server/main.c
@@ -1,7 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
 #include <shared/network.h>
+
+#define BUFSIZE 128
+
+static int poll_network(int fd)
+{
+	static char buf[BUFSIZE];
+	struct sockaddr_storage addr;
+	socklen_t addrlen = sizeof(addr);
+	ssize_t ret;
+
+	ret = recvfrom(fd, buf, BUFSIZE, MSG_TRUNC, (struct sockaddr*)&addr, &addrlen);
+	if (ret == -1) {
+		fprintf(stderr, "recvfrom(): %s\n", strerror(errno));
+		return 0;
+	}
+
+	if (ret > BUFSIZE) {
+		fprintf(stderr, "recvfrom(): Message truncated, more than %li bytes received.\n",
+			(long)ret);
+		return 0;
+	}
+
+	printf("Received packet of size %li\n", (long)ret);
+	return 1;
+}
 
 int main(int argc, char **argv)
 {
@@ -21,6 +51,10 @@ int main(int argc, char **argv)
 	 *   2) logic(from, event, to)
 	 *   3) push_network(network, to)
 	 */
+
+	while (1) {
+		poll_network(fd);
+	}
 
 	close(fd);
 
